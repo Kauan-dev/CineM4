8ad72fbceeb660214956eaaa0add4c1c3d9638bf
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-08-26 18:57:06 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-amber-500">CineM4</h1>
+    </main>
+  );
+}
+
+export default App;
