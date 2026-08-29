631162ce07fe3fa2053eb49e3cae02e1fcfa0878
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-08-29 01:16:40 -0300

    style: ajusta altura mínima do main

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,7 +13,9 @@ function Layout({ children }: LayoutProps) {
     <>
       <Header />
 
-      <main className="mt-15 py-4 lg:mb-0">{children}</main>
+      <main className="mt-15 min-h-[calc(100dvh-259px)] py-4 lg:mb-0 lg:min-h-[calc(100vh-137px)]">
+        {children}
+      </main>
 
       <TabBar />
 
