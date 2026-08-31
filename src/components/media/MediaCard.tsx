41197fadd41818a87d316bacc1f5470658931edf
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-08-31 19:55:26 -0300

    feat: adiciona componente MediaCard

---
+++ src/components/media/MediaCard.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+import type { Movie, Series } from "../../services/tmdb/types";
+
+interface MediaCardProps {
+  media: Movie | Series;
+}
+
+export function MediaCard({ media }: MediaCardProps) {
+  const isMovie = "title" in media;
+  const title = isMovie ? media.title : media.name;
+  const type = isMovie ? "movie" : "series";
+
+  return (
+    <Link
+      to={`/${type}/${media.id}`}
+      title={title}
+      className="block w-36 shrink-0 rounded-sm outline-2 outline-offset-3 outline-transparent transition-all duration-300 ease-in-out hover:outline-neutral-50 focus-visible:outline-neutral-50"
+    >
+      {media.poster_path && (
+        <img
+          className="aspect-2/3 w-full rounded-sm object-cover"
+          src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
+          title={title}
+          alt={title}
+          loading="lazy"
+        />
+      )}
+    </Link>
+  );
+}
