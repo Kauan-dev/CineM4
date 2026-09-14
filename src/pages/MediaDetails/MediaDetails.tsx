e126f70ca3bf4b0b4514ae03e570198353f70c66
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-09-13 23:57:55 -0300

    feat: adiciona backdrop e informações básicas à página de detalhes

--- src/pages/MediaDetails/MediaDetails.tsx
+++ src/pages/MediaDetails/MediaDetails.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-
 import { getMovieDetails } from "@/services/tmdb/movies";
 import { getSeriesDetails } from "@/services/tmdb/series";
 import type { MediaDetailsData } from "@/services/tmdb/types";
@@ -31,9 +30,41 @@ export function MediaDetails() {
 
   const title = "title" in media ? media.title : media.name;
 
+  const year = new Date(
+    "release_date" in media ? media.release_date : media.first_air_date,
+  ).toLocaleDateString("pt-BR", { year: "numeric" });
+
+  const runtimeOrSeasons =
+    "runtime" in media
+      ? media.runtime
+        ? `${Math.floor(media.runtime / 60)}h ${media.runtime % 60}m`
+        : null
+      : `${media.number_of_seasons} ${
+          media.number_of_seasons === 1 ? "temporada" : "temporadas"
+        }`;
+
   return (
     <main>
+      <div className="relative w-full">
+        <img
+          src={`https://image.tmdb.org/t/p/original${media.backdrop_path}`}
+          alt=""
+          className="h-[36vh] w-full object-cover object-[50%_30%] sm:h-[50vh] md:h-[62vh] lg:h-[72vh]"
+        />
+
+        <div className="absolute inset-0 bg-linear-to-t from-black via-black/50 to-transparent" />
+      </div>
+
       <h1>{title}</h1>
+
+      <div className="flex gap-1">
+        <div>{year}</div>
+        <span>-</span>
+        <div>{runtimeOrSeasons}</div>
+      </div>
+
+      <div>{media.genres.map((genre) => genre.name).join(", ")}</div>
+
       <p>{media.overview}</p>
     </main>
   );
--- src/services/tmdb/types.ts
+++ src/services/tmdb/types.ts
@@ -7,7 +7,7 @@ export interface Movie {
   id: number;
   title: string;
   poster_path: string | null;
-  backdrop_path?: string | null;
+  backdrop_path: string | null;
   release_date: string;
   vote_average: number;
 }
@@ -23,7 +23,7 @@ export interface Series {
   id: number;
   name: string;
   poster_path: string | null;
-  backdrop_path?: string | null;
+  backdrop_path: string | null;
   first_air_date: string;
   vote_average: number;
 }
